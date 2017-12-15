d30635ac4283a2b51524f6031fff9465060e5333
particleflux <particleflux@gmail.com>
2017-12-15 19:33:21 +0100

    Initial commit

---
+++ DirectImage.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         DirectImage
+// @namespace    http://particleflux.codes/
+// @version      1.0.0
+// @description  Directly open images for image hosting sites
+// @author       particleflux
+// @match        http*://suckmypic.net/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
